5aa42136fcaea256de7f09af04883ffd4269449f
boescpat <patboe@gmail.com>
2021-11-16 16:38:23 +0100

    Responsive design for main pages.

---
+++ misc/script-main-elements.js
@@ -0,0 +1,16 @@
+/* Fetch header and footer */
+fetch("misc/header-main.html").then(x => x.text()).then(y => document.getElementById("header-main").innerHTML = y);
+fetch("misc/footer-main.html").then(x => x.text()).then(y => document.getElementById("footer-main").innerHTML = y);
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function toggleBurgerMenu() {
+    var burgerMenu = document.getElementById("burgerMenu");
+    var overlay = document.getElementById("overlay");
+    if (burgerMenu.style.display === "block") {
+        burgerMenu.style.display = "none";
+        overlay.style.display = "none";
+    } else {
+        burgerMenu.style.display = "block";
+        overlay.style.display = "block";
+    }
+} 
