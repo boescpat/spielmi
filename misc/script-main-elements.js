c7bd321051225beccd2708834dbfb60a3cded972
boescpat <patboe@gmail.com>
2021-11-17 09:58:23 +0100

    Clean up of basic structure-work.

--- misc/script-main-elements.js
+++ misc/script-main-elements.js
@@ -1,6 +1,6 @@
 /* Fetch header and footer */
-fetch("misc/header-main.html").then(x => x.text()).then(y => document.getElementById("header-main").innerHTML = y);
-fetch("misc/footer-main.html").then(x => x.text()).then(y => document.getElementById("footer-main").innerHTML = y);
+fetch("https://spielmi.ch/misc/header-main.html").then(x => x.text()).then(y => document.getElementById("header-main").innerHTML = y);
+fetch("https://spielmi.ch/misc/footer-main.html").then(x => x.text()).then(y => document.getElementById("footer-main").innerHTML = y);
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function toggleBurgerMenu() {
